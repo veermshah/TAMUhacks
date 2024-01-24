9f086470aed6ef3f4a47e6bae6fff25ef8e60b98
Veer Shah <veermickey@gmail.com>
2024-01-24 14:59:10 -0600

    pie chart and react build. header/navbar still needs work

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PortfolioChart from './components/PortfolioChart';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World!</p>
+      <PortfolioChart />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/PortfolioChart.js
@@ -0,0 +1,64 @@
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { useState } from "react";
+
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title
+);
+
+function PortfolioChart() {
+    const data = {
+        labels: ['Stocks', 'Bonds', 'Cash', 'Equity', 'Real Estate'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+    
+    return (
+        <div className="App">
+            <h1>Portfolio Chart Below</h1>
+            <div className="p-2 max-w-[400px]">
+                <Pie data={data}></Pie>
+            </div>
+        </div>
+    );
+}
+
+export default PortfolioChart;
