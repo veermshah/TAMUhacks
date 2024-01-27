b6726c34a1ddf6c74e261d512e6d4d5676ad25dc
Veer Shah <veermickey@gmail.com>
2024-01-27 11:30:13 -0600

    removed a link from framer motion

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -93,15 +93,17 @@ export default function Home() {
                 </div>
             </div>
             <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-                <a href="#about">
-                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
-                        <motion.dev
-                        animate={{y:[0,24,0]}}
-                        transition={{repeat:Infinity, duration:1.5, repeatType: 'loop'}}
+                <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
+                    <motion.dev
+                        animate={{ y: [0, 24, 0] }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 1.5,
+                            repeatType: "loop",
+                        }}
                         className="w-3 h-3 rounded-full bg-white mb-1"
-                        />
-                    </div>
-                </a>
+                    />
+                </div>
             </div>
         </>
     );
