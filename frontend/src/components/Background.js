7530d3dbf20f6c9e053c1fb51c0e73ef3aeb0788
Veer Shah <84329659+veermshah@users.noreply.github.com>
2024-01-28 04:49:51 -0600

    Update Background.js

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -1,19 +1,24 @@
-// Background.js
+// BackgroundVideo.js
 
 import React from "react";
+import waterfall from "../waterfall.mp4";
 
-const Background = () => {
+const BackgroundVideo = () => {
   return (
     <div className="bg-cover bg-center fixed inset-0 z-[-1]">
-      {/* Add your GIF file as the background image */}
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage: `url("/path/to/your/background.gif")`,
-        }}
-      />
+      {/* Add your video file as the background video */}
+      <video
+        className="absolute inset-0 object-cover w-full h-full"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={waterfall} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute inset-0 bg-black opacity-80"></div>
     </div>
   );
 };
 
-export default Background;
+export default BackgroundVideo;
