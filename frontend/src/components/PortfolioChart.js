da91a471ee38dd5820a58035759b0b5ff29fb488
Veer Shah <84329659+veermshah@users.noreply.github.com>
2024-01-28 04:51:07 -0600

    Update PortfolioChart.js

--- frontend/src/components/PortfolioChart.js
+++ frontend/src/components/PortfolioChart.js
@@ -7,7 +7,7 @@ import {
     LinearScale,
     PointElement,
     LineElement,
-    Title
+    Title,
 } from "chart.js";
 import { Pie } from "react-chartjs-2";
 
@@ -24,37 +24,68 @@ ChartJS.register(
 
 function PortfolioChart() {
     const data = {
-        labels: ['Stocks', 'Bonds', 'Cash', 'Equity', 'Real Estate'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
+        labels: [
+            "Communication Services",
+            "Consumer Discretionary",
+            "Consumer Staples",
+            "Energy",
+            "Financials",
+            "Health Care",
+            "Industrials",
+            "Information Technology",
+            "Materials",
+            "Real Estate",
+            "Utilities",
         ],
-      };
-    
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3, 23, 5, 2, 3, 2],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const options = {
+        plugins: {
+            title: {
+                display: true,
+                text: "Your Portfolio Allocations",
+                font: {
+                    size: 18,
+                },
+            },
+        },
+        layout: {
+            padding: {
+                left: 20,
+                right: 20,
+                top: 0,
+                bottom: 0,
+            },
+        },
+    };
+
     return (
         <div className="App">
-            <h1>Portfolio Chart Below</h1>
-            <div className="p-2 max-w-[600px]">
-                <Pie data={data}></Pie>
+            <div className="min-w-[600px] w-3/7">
+                <Pie data={data} options={options}></Pie>
             </div>
         </div>
     );
